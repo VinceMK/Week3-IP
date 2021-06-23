86d316b91904b1de2d4d32c3df459aac70373341
Vincent <vincent.mwangi@student.moringaschool.com>
2021-06-23 15:32:47 +0300

    contact form js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
-
+    
     $('#details').click(function() {
         $('#details').hide();
         $('#design').show();
         $('#design').click(function(){
             $('#design').hide();
             $('#details').show();
+
+        });
+
+    });
+
+    $('#details2').click(function() {
+        $('#details2').hide();
+        $('#development').show();
+        $('#development').click(function(){
+            $('#development').hide();
+            $('#details2').show();
+
+        });
+
+    });
+
+    $('#details3').click(function() {
+        $('#details3').hide();
+        $('#product').show();
+        $('#product').click(function(){
+            $('#product').hide();
+            $('#details3').show();
 
         });
 
